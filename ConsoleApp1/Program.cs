684811cc2e958fc7e2bad41b2c68a7148d22f649
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-05-08 21:06:21 +0300

    OpenOrSenior()

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ConditionalLoop;
-using CodeWarsExercises;
 
 namespace ConsoleApp
 {
@@ -12,7 +8,23 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine(string.Join(" " ,OpenOrSenior(new[] { new[] { 3, 12 }, new[] { 55, 1 }, new[] { 91, -2 }, new[] { 54, 23 } })));
+            Console.ReadLine();
+        }
+        public static IEnumerable<string> OpenOrSenior(int[][] data)
+        {
+            List<string> subs = new List<string>();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i][0] >= 55 && (data[i][1] >= 7 && data[i][1] <= 26))
+                {
+                    subs.Add("Senior");
+                }
+                else
+                    subs.Add("Open");
+            }
+            return subs;
         }
     }
 }
