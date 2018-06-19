f3aea93a8a7904992490d34cef0beca5ba455251
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-19 19:15:34 +0300

    added Animal.cs

---
+++ Animals/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int AnimalID { get; set; }
+
+        public Animal()
+        {
+
+        }
+        public Animal(string name = "No Name", double weight = 0, double height = 0)
+        {
+            Name = name;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
