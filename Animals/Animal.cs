1b5707b740473a300096c6833765f85d6fcf8bdd
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-19 19:53:56 +0300

    added Run.cs classs with Main method

--- Animals/Animal.cs
+++ Animals/Animal.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;//am nevoie de namesapce-ul asta daca vreau sa foloses serializarea
+using System.Runtime.Serialization.Formatters.Binary;//am nevoie de namespace-ul asta pentru serializare
 
 namespace Animals
 {
-    public class Animal
+    //pentru inceput trebuie sa adaug atributul clasei ce o face sa fie serializabila
+    [Serializable]
+    public class Animal : ISerializable // dupa trebuie sa implementam interfata asta ce ne aduce o metoda cu care o sa facem serializarea 
     {
         public string Name { get; set; }
         public double Weight { get; set; }
@@ -25,7 +29,29 @@ namespace Animals
         }
         public override string ToString()
         {
-            return string.Format("{0} weights: {1} and is {2} tall.",Name,Weight,Height);
+            return string.Format("{0} weights: {1} and is {2} tall.", Name, Weight, Height);
+        }
+        /// <summary>
+        /// Metoda asta sa ne ajute sa facem serializarea 
+        /// </summary>
+        /// <param name="info">o sa folosim acest parametru pentru a adauga valori in fisierul nostru serializat</param>
+        /// <param name="context"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Weight", Weight);
+            info.AddValue("Height", Height);
+        }
+        /// <summary>
+        /// Constructorul asta o sa ne ajute a deserializare
+        /// </summary>
+        /// <param name="info">parametru ce va aduce valoarea serializata si o va converti in obiect</param>
+        /// <param name="context"></param>
+        public Animal(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Weight = (double)info.GetValue("Weight", typeof(double));
+            Height = (double)info.GetValue("Height", typeof(double));
         }
     }
 }
---
+++ Animals/Run.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//acesta o sa ne ajute sa scriem catre un fisier
+using System.Runtime.Serialization.Formatters.Binary;// tot asa ca sa citim serializarile 
+using System.Xml.Serialization;//ca sa serilizam in XML
+namespace Animals
+{
+    class Run
+    {
+        public void Main(string[] args)
+        {
+            Animal bowser = new Animal("Bowser", 34, 23);
+
+            //ca sa serilizam catre un fisier facem
+
+            Stream stream = File.Open("AnimalData.dat", FileMode.Create);//daca fisierul AnimalData nu exista, o sa-l creeze al doilea parametru 
+            //dat = este un tip generic, se poate salva orice in el si este preferat in DLL
+
+            Console.ReadKey();
+        }
+    }
+}
